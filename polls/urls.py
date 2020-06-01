55ab02a8bfc6d02f19fcea4b721451a5768cc0f0
galahad <asj931@yahoo.com>
2020-06-01 11:42:48 +0300

    импорт исправлен + мелкие улучшения

--- polls/urls.py
+++ polls/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from polls import views
 
 urlpatterns = [
-    path('', views.mapp, name='mapp'),
+    path('', views.reset, name='reset'),
     path('get_value', views.get_value, name='get_value'),
     path('reset', views.reset, name='reset'),
-    path('intoDb', views.reset, name='intoDb')
+    path('intoDb', views.intoDb, name='intoDb')
 ]
--- polls/views.py
+++ polls/views.py
@@ -3,43 +3,48 @@ from django.shortcuts import render
 import folium
 from bs4 import BeautifulSoup
 import math
-from django.http import HttpResponse
 
+
+# добавление block и endblock элементов в html с картой
 def block():
     with open('templates/map1.html') as inf:
         txt = inf.read()
-        soup = BeautifulSoup(txt)
+        soup = BeautifulSoup(txt, "html.parser")
     block_cont = '{% block content %}'
     end_cont = '{% endblock %}'
-    # insert it into the document
+
     soup.append(block_cont)
     soup.append(end_cont)
 
-    # save the file again
     with open('templates/map1.html', "w") as outf:
         outf.write(str(soup))
 
 
-mappy = folium.Map(width=950, height=950, location=[59.939095, 30.315856], zoom_start=11)
-mappy.save('templates/map1.html')
-block()
-
-
-def reset(request):
-    map = folium.Map(width=950, height=950, location=[59.939095, 30.315856], zoom_start=11)
-    mappy = map
+# отчистка от маркеров
+def map():
+    mappy = folium.Map(width=950, height=950, location=[59.939095, 30.315856], zoom_start=11)  # карта
     mappy.save('templates/map1.html')
     block()
+    return mappy
+
+
+# сброс маркеров и значиней в текстбоксах
+def reset(request):
+    map()
+    lst.clear()
     return render(request, 'base.html')
 
 
+# добавление маркеров нв карту
 def marker(cord1_1, cord1_2, cord2_1, cord2_2):
+    mappy = map()
     folium.Marker(location=[cord1_1, cord1_2], popup="Метка1", icon=folium.Icon(color='red')).add_to(mappy)
     folium.Marker(location=[cord2_1, cord2_2], popup="Метка2", icon=folium.Icon(color='blue')).add_to(mappy)
     mappy.save('templates/map1.html')
     block()
 
 
+# расчет расстояния между точками
 def way(cord1_1, cord1_2, cord2_1, cord2_2):
     rad = 6372795
     # в радианах
@@ -61,52 +66,56 @@ def way(cord1_1, cord1_2, cord2_1, cord2_2):
     y = math.sqrt(math.pow(cl2 * sdelta, 2) + math.pow(cl1 * sl2 - sl1 * cl2 * cdelta, 2))
     x = sl1 * sl2 + cl1 * cl2 * cdelta
     ad = math.atan2(y, x)
-    dist = ad * rad
-    dist = round(dist / 1000)
+    dist = round(ad * rad / 1000)
 
     return dist
 
 
+# запись координат и расстояния в бд
 def intoDb(request):
-    con = psycopg2.connect(
-        database="django_db",
-        user="user_name",
-        password="12345",
-        host="127.0.0.1",
-        port="5432"
-    )
-
-    print("Database opened successfully")
-    cur = con.cursor()
-    cur.execute(
-        "INSERT INTO cooords (long1,width1,long2,width2,total) VALUES (%s,%s,%s,%s,%s)",
-        (float(request.POST.get('cord1_1')), float(request.POST.get('cord1_2')), float(request.POST.get('cord2_1')), float(request.POST.get('cord2_2')), lst[4])
-    )
-
-    con.commit()
-    print("Record inserted successfully")
-
-    con.close()
-    return render(request,'base.html')
-
-
-
-lst = []
-
-
-def get_value(request):
     try:
-        lst.append(request.GET.get('cord1.1'))
-        lst.append(request.GET.get('cord1.2'))
-        lst.append(request.GET.get('cord2.1'))
-        lst.append(request.GET.get('cord2.2'))
-        lst.append(way(float(lst[0]), float(lst[1]), float(lst[2]), float(lst[3])))
-        marker(lst[0], lst[1], lst[2], lst[3])
-        return render(request, 'base.html', {'cord1_1': lst[0],'cord1_2': lst[1],'cord2_1': lst[2],'cord2_2': lst[3]})
-    except ValueError:
+        map()
+        con = psycopg2.connect(
+            database="django_db",
+            user="user_name",
+            password="12345",
+            host="127.0.0.1",
+            port="5432"
+        )
+
+        print("Database opened successfully")
+        cur = con.cursor()
+        cur.execute(
+            "INSERT INTO cooords (long1,width1,long2,width2,total) VALUES (%s,%s,%s,%s,%s)",
+            (float(lst[0]), float(lst[1]), float(lst[2]),
+             float(lst[3]), lst[4])
+        )
+
+        con.commit()
+        print("Record inserted successfully")
+
+        con.close()
+        lst.clear()
+        return render(request, 'base.html')
+    except IndexError:
         return render(request, 'base.html')
 
 
-def mapp(request):
-    return render(request, 'base.html')
+lst = []  # список в котором временно хранятся координаты и расстояние
 
+
+#  получение значений которые были введены в текстбоксы
+def get_value(request):
+    try:
+        lst.clear()
+        map()
+        lst.append(request.POST.get('cord1.1'))
+        lst.append(request.POST.get('cord1.2'))
+        lst.append(request.POST.get('cord2.1'))
+        lst.append(request.POST.get('cord2.2'))
+        lst.append(way(float(lst[0]), float(lst[1]), float(lst[2]), float(lst[3])))
+        marker(lst[0], lst[1], lst[2], lst[3])
+        return render(request, 'base.html',
+                      {'cord1_1': lst[0], 'cord1_2': lst[1], 'cord2_1': lst[2], 'cord2_2': lst[3], 'long': lst[4]})
+    except ValueError:
+        return render(request, 'base.html')
